81628d60d1fcec9c5ecf51b59beeb74f75eb62ce
csd4ni3l <96988024+csd4ni3l@users.noreply.github.com>
2026-07-17 17:31:39 +0200

    fix secureboot feature missing outer layer

--- modules/features/secureboot.nix
+++ modules/features/secureboot.nix
@@ -1,13 +1,17 @@
-{pkgs, lib, ...}: {
-  environment.systemPackages = [
-    pkgs.sbctl
-  ];
+{self, ...}: {
+  flake.nixosModules.secureboot = {
+    pkgs, lib, ...
+  }: {
+    environment.systemPackages = [
+      pkgs.sbctl
+    ];
 
-  boot.loader.limine = {
-    enable = true;
-    secureBoot.enable = true;
-    enrollConfig = true;
-    validateChecksums = true;
-    panicOnChecksumMismatch = true;
+    boot.loader.limine = {
+      enable = true;
+      secureBoot.enable = true;
+      enrollConfig = true;
+      validateChecksums = true;
+      panicOnChecksumMismatch = true;
+    };
   };
 }
