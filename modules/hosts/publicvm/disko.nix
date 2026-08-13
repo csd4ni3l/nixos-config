70e54f2528f970e9ec7312f426b4482eae9b286b
csd4ni3l <csd4ni3l_contact.ladle014@passmail.com>
2026-08-13 14:45:26 +0200

    Use /dev/sda instead of /dev/vda for the disko install of the VMs, as that is the default in Proxmox

--- modules/hosts/publicvm/disko.nix
+++ modules/hosts/publicvm/disko.nix
@@ -4,7 +4,7 @@
     disko.devices = {
       disk.main = {
         type = "disk";
-        device = "/dev/vda";
+        device = "/dev/sda";
         content = {
           type = "gpt";
           partitions = {
