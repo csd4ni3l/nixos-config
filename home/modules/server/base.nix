4d8ea1edd68a6efa1dc5ca77638b65b8de542042
csd4ni3l <csd4ni3l_contact.ladle014@passmail.com>
2026-08-14 18:24:10 +0200

    add reloadquadletunits user service which fixes the default.target race that causes containers not to start on reboot

--- home/modules/server/base.nix
+++ home/modules/server/base.nix
@@ -24,6 +24,25 @@
       ${lib.concatMapStringsSep "\n" (d: "mkdir -p '${d}'") config.homelab.containerDirs}
     '';
 
+    home.activation.reloadQuadletUnits = lib.hm.dag.entryAfter ["writeBoundary" "sops-nix"] ''
+      if [[ -d "/run/user/$(id -u)/systemd" ]]; then
+        export XDG_RUNTIME_DIR="/run/user/$(id -u)"
+        systemctl --user daemon-reload
+        restart=false
+        for c in '${config.home.homeDirectory}'/.config/containers/systemd/*.container; do
+          [[ -e "$c" ]] || continue
+          unit="$(basename "$c" .container).service"
+          if ! systemctl --user is-active --quiet "$unit" 2>/dev/null; then
+            restart=true
+            break
+          fi
+        done
+        if [[ "$restart" == true ]]; then
+          systemctl --user restart default.target
+        fi
+      fi
+    '';
+
     home.username = "deploy";
     home.homeDirectory = "/home/deploy";
     home.stateVersion = "26.11";
