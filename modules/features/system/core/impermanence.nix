b70b7b990d445cd22cf14ade85394e437ab7ca9f
csd4ni3l <csd4ni3l_contact.ladle014@passmail.com>
2026-08-14 17:45:44 +0200

    persist /etc/ssh so host keys stay the same on both VMs, framework16 doesnt have SSH

--- modules/features/system/core/impermanence.nix
+++ modules/features/system/core/impermanence.nix
@@ -4,6 +4,7 @@
     environment.persistence."/persist" = {
       hideMounts = true;
       directories = [
+        "/etc/ssh"
         "/etc/NetworkManager/system-connections"
         "/var/lib/bluetooth"
         "/var/lib/nixos"
