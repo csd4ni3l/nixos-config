4b5a719af20fa0db5e8e932b2071fc37e501a162
csd4ni3l <csd4ni3l_contact.ladle014@passmail.com>
2026-08-17 13:21:43 +0200

    switch to forgejo rootless image and use app.ini configuration instead of environment variables

--- home/modules/server/containers/forgejo.nix
+++ home/modules/server/containers/forgejo.nix
@@ -1,11 +1,68 @@
 {config, ...}: {
-  homelab.containerDirs = ["${config.home.homeDirectory}/containers/forgejo/data"];
+  homelab.containerDirs = ["${config.home.homeDirectory}/containers/forgejo/data" "${config.home.homeDirectory}/containers/forgejo/config"];
 
   sops.secrets."forgejo-user-password" = {};
   sops.secrets."forgejo-domain" = {};
   sops.secrets."forgejo-lfs-jwt-secret" = {};
   sops.secrets."forgejo-internal-token" = {};
   sops.secrets."forgejo-oauth2-secret" = {};
+
+  sops.templates."forgejo-app-ini" = {
+    path = "${config.home.homeDirectory}/containers/forgejo/config/app.ini";
+    content = ''
+      APP_NAME=csd4ni3l Git
+
+      [database]
+      DB_TYPE=sqlite3
+      HOST=127.0.0.1:3306
+      NAME=forgejo
+      USER=csd4ni3l
+      PASSWD=${config.sops.placeholder."forgejo-user-password"}
+      PATH=/var/lib/gitea/forgejo.db
+
+      [server]
+      SSH_DOMAIN=${config.sops.placeholder."forgejo-domain"}
+      DOMAIN=${config.sops.placeholder."forgejo-domain"}
+      HTTP_PORT=3000
+      ROOT_URL=https://${config.sops.placeholder."forgejo-domain"}
+      SSH_PORT=22
+      LFS_START_SERVER=true
+      LFS_JWT_SECRET=${config.sops.placeholder."forgejo-lfs-jwt-secret"}
+      OFFLINE_MODE=true
+      DISABLE_SSH=true
+
+      [service]
+      REGISTER_EMAIL_CONFIRM=false
+      ENABLE_NOTIFY_MAIL=false
+      DISABLE_REGISTRATION=true
+      ALLOW_ONLY_EXTERNAL_REGISTRATION=false
+      ENABLE_CAPTCHA=true
+      REQUIRE_SIGNIN_VIEW=false
+      DEFAULT_KEEP_EMAIL_PRIVATE=true
+      DEFAULT_ALLOW_CREATE_ORGANIZATION=true
+      DEFAULT_ENABLE_TIMETRACKING=true
+      NO_REPLY_ADDRESS=noreply.localhost
+
+      [security]
+      INSTALL_LOCK=true
+      INTERNAL_TOKEN=${config.sops.placeholder."forgejo-internal-token"}
+      PASSWORD_HASH_ALGO=pbkdf2_hi
+
+      [oauth2]
+      JWT_SECRET=${config.sops.placeholder."forgejo-oauth2-secret"}
+      ENABLED=false
+
+      [openid]
+      ENABLE_OPENID_SIGNIN=false
+      ENABLE_OPENID_SIGNUP=false
+
+      [packages]
+      ENABLED=true
+
+      [mailer]
+      ENABLED=false
+    '';
+  };
 
   sops.templates."forgejo-container" = {
     path = "${config.home.homeDirectory}/.config/containers/systemd/forgejo.container";
@@ -15,59 +72,15 @@
       After=network-online.target
 
       [Container]
-      UserNS=keep-id:uid=1001,gid=100
+      UserNS=keep-id:uid=1000,gid=1000
       ContainerName=forgejo
       AutoUpdate=registry
-      Image=codeberg.org/forgejo/forgejo:16
-
-      Environment=USER_UID=1001
-      Environment=USER_GID=100
-
-      Environment=FORGEJO_APP_NAME="csd4ni3l Git"
-
-      Environment=FORGEJO__database__DB_TYPE=sqlite3
-      Environment=FORGEJO__database__HOST=127.0.0.1:3306
-      Environment=FORGEJO__database__NAME=forgejo
-      Environment=FORGEJO__database__USER=csd4ni3l
-      Environment=FORGEJO__database__PASSWD=${config.sops.placeholder."forgejo-user-password"}
-      Environment=FORGEJO__database__PATH=/data/forgejo.db
-
-      Environment=FORGEJO__server__SSH_DOMAIN=${config.sops.placeholder."forgejo-domain"}
-      Environment=FORGEJO__server__DOMAIN=${config.sops.placeholder."forgejo-domain"}
-      Environment=FORGEJO__server__HTTP_PORT=3000
-      Environment=FORGEJO__server__ROOT_URL=https://${config.sops.placeholder."forgejo-domain"}
-      Environment=FORGEJO__server__SSH_PORT=22
-      Environment=FORGEJO__server__LFS_START_SERVER=true
-      Environment=FORGEJO__server__LFS_JWT_SECRET=${config.sops.placeholder."forgejo-lfs-jwt-secret"}
-      Environment=FORGEJO__server__OFFLINE_MODE=true
-
-      Environment=FORGEJO__service__REGISTER_EMAIL_CONFIRM=false
-      Environment=FORGEJO__service__ENABLE_NOTIFY_MAIL=false
-      Environment=FORGEJO__service__DISABLE_REGISTRATION=true
-      Environment=FORGEJO__service__ALLOW_ONLY_EXTERNAL_REGISTRATION=false
-      Environment=FORGEJO__service__ENABLE_CAPTCHA=true
-      Environment=FORGEJO__service__REQUIRE_SIGNIN_VIEW=false
-      Environment=FORGEJO__service__DEFAULT_KEEP_EMAIL_PRIVATE=true
-      Environment=FORGEJO__service__DEFAULT_ALLOW_CREATE_ORGANIZATION=true
-      Environment=FORGEJO__service__DEFAULT_ENABLE_TIMETRACKING=true
-      Environment=FORGEJO__service__NO_REPLY_ADDRESS=noreply.localhost
-
-      Environment=FORGEJO__security__INSTALL_LOCK=true
-      Environment=FORGEJO__security__INTERNAL_TOKEN=${config.sops.placeholder."forgejo-internal-token"}
-      Environment=FORGEJO__security__PASSWORD_HASH_ALGO=pbkdf2_hi
-
-      Environment=FORGEJO__oauth2__JWT_SECRET=${config.sops.placeholder."forgejo-oauth2-secret"}
-
-      Environment=FORGEJO__openid__ENABLE_OPENID_SIGNIN=false
-      Environment=FORGEJO__openid__ENABLE_OPENID_SIGNUP=false
-      Environment=FORGEJO__packages__ENABLED=true
-      Environment=FORGEJO__oauth2__ENABLED=false
-      Environment=FORGEJO__server__DISABLE_SSH=true
-      Environment=FORGEJO__mailer__ENABLED=false
+      Image=codeberg.org/forgejo/forgejo:16-rootless
 
       PublishPort=127.0.0.1:59001:3000
 
-      Volume=%h/containers/forgejo/data:/data:Z
+      Volume=%h/containers/forgejo/config:/etc/gitea:Z
+      Volume=%h/containers/forgejo/data:/var/lib/gitea:Z
       Volume=/etc/localtime:/etc/localtime:ro
 
       [Install]
