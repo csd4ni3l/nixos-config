9e92d97f749002675d6a4e00ab06964255a36a67
csd4ni3l <csd4ni3l_contact.ladle014@passmail.com>
2026-08-19 20:58:58 +0200

    remove navidrome network, remove aonsoku env variables, aonsoku connects from the client, which means internal does not work

--- home/modules/server/containers/navidrome.nix
+++ home/modules/server/containers/navidrome.nix
@@ -119,11 +119,6 @@
         main()
   '';
 
-  home.file.".config/containers/systemd/navidrome.network".text = ''
-    [Network]
-    NetworkName=navidrome
-  '';
-
   home.file.".config/containers/systemd/navidrome.container".text = ''
     [Unit]
     Description=Navidrome Container
@@ -133,7 +128,6 @@
     ContainerName=navidrome
     AutoUpdate=registry
     UserNS=keep-id
-    Network=navidrome.network
     Image=docker.io/deluan/navidrome:latest
 
     Volume=%h/containers/navidrome/data:/data:Z
