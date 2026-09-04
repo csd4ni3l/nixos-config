fe07ca362803e5f67e2781126bd7a2394eee2178
csd4ni3l <csd4ni3l_contact.ladle014@passmail.com>
2026-09-04 23:26:18 +0200

    fix pelican wings ssl issues

--- home/modules/server/containers/pelican-wings.nix
+++ home/modules/server/containers/pelican-wings.nix
@@ -1,4 +1,4 @@
-{config, ...}: {
+{ config, pkgs, ... }: {
   homelab.containerDirs = [
     "${config.home.homeDirectory}/containers/wings/etc"
     "${config.home.homeDirectory}/containers/wings/lib"
@@ -10,7 +10,6 @@
     ".config/containers/systemd/wings.network".text = ''
       [Unit]
       Description=Wings Network
-
       [Network]
       Subnet=172.21.0.0/16
       NetworkName=wings0
@@ -20,34 +19,26 @@
       [Unit]
       Description=Pelican Wings
       After=network-online.target
-
       [Container]
       ContainerName=pelican-wings
       AutoUpdate=registry
       Image=ghcr.io/pelican/wings:latest
       Network=wings.network
-
       Environment=TZ=Europe/Budapest
       Environment=WINGS_UID=1002
       Environment=WINGS_GID=100
       Environment=WINGS_USERNAME=pelican
-
       PublishPort=127.0.0.1:54001:8080
-
-      # Point at Podman's docker-compatible rootless socket instead of dockerd
       Volume=%t/podman/podman.sock:/var/run/docker.sock
       Volume=%h/.local/share/containers/storage:/var/lib/docker/containers/:ro
       Volume=%h/containers/wings/etc:/etc/pelican/
       Volume=%h/containers/wings/lib:/var/lib/pelican/
       Volume=%h/containers/wings/log:/var/log/pelican/
       Volume=%h/containers/wings/tmp:/tmp/pelican/
-      Volume=/etc/ssl/certs:/etc/ssl/certs:ro
-
+      Volume=${pkgs.nss-cacert}/etc/ssl/certs/ca-bundle.crt:/etc/host-ca-bundle.crt:ro
       PodmanArgs=--tty
-
       [Service]
       Restart=always
-
       [Install]
       WantedBy=default.target
     '';
