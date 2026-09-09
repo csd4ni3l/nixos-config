55fcadf68488390496c09216a8f81bde1a043f4e
csd4ni3l <csd4ni3l_contact.ladle014@passmail.com>
2026-09-09 17:37:45 +0200

    fix prismlauncher not launching minecraft because minecraft required x11

--- modules/features/jail.nix/programs/gaming/prism-launcher.nix
+++ modules/features/jail.nix/programs/gaming/prism-launcher.nix
@@ -10,6 +10,8 @@
       (jail.mkSandboxed pkgs.prismlauncher "prismlauncher" (with jail.combinators; [
         default
         network
+        # NOTE: needs X11 for minecraft
+        unsafe-x11
 
         (rw-bind (noescape "~/.local/share/PrismLauncher") (noescape "~/.local/share/PrismLauncher"))
       ]))
