b2f8ced87b648b3928d75183779c7ed96f9ea94b
csd4ni3l <csd4ni3l_contact.ladle014@passmail.com>
2026-09-18 21:01:22 +0200

    add newt to anotherpublicvm

--- home/guest-anotherpublicvm.nix
+++ home/guest-anotherpublicvm.nix
@@ -1,4 +1,5 @@
 # NOTE: guest is the user that has access to a rootless podman socket, and runs untrusted workloads, so it is separated from deploy for security
+# normally, guest would not run newt, but i dont want to use the deploy user just for newt here
 {self, ...}: {
   home.username = "guest";
   home.homeDirectory = "/home/guest";
@@ -7,6 +8,7 @@
     self.homeModules.options
     ./modules/common/default.nix
     ./modules/server/base.nix
+    ./modules/server/services/newt.nix
     ./modules/server/services/forgejo-runner.nix
     ./modules/server/services/wings.nix
   ];
