19dab2e8b0258bebaf63f52ea9e70b24494607c9
csd4ni3l <csd4ni3l_contact.ladle014@passmail.com>
2026-09-19 18:27:15 +0200

    improve Firefox preferences

--- home/modules/desktop/browsing.nix
+++ home/modules/desktop/browsing.nix
@@ -29,7 +29,7 @@
       extraConfig =
         builtins.readFile "${inputs.arkenfox}/user.js"
         + ''
-          // Custom settings
+          // urlbar stuff
           user_pref("browser.search.suggest.enabled", false);
           user_pref("browser.urlbar.suggest.searches", false);
           user_pref("browser.urlbar.suggest.bookmark", false);
@@ -40,12 +40,25 @@
           user_pref("browser.urlbar.quicksuggest.sponsoredEnabled", false);
           user_pref("browser.urlbar.quicksuggest.nonSponsoredEnabled", false);
           user_pref("browser.urlbar.quicksuggest.dataCollection.enabled", false);
+
+          // extra OPSEC or whatever
+          user_pref("permissions.memory_only", true);
+          user_pref("security.nocertdb", true);
+          user_pref("browser.download.forbid_open_with", true);
+          user_pref("dom.popup_allowed_events", "click dblclick mousedown pointerdown");
+          user_pref("javascript.options.asmjs", false);
+
+          // ew DRM
+          user_pref("media.eme.enabled", false);
+          user_pref("media.eme.ui.enabled", false);
+
           // password manager and autofill off (Bitwarden handles it)
           user_pref("signon.rememberSignons", false);
           user_pref("signon.autofillForms", false);
           user_pref("signon.storeWhenAutocompleteOff", false);
           user_pref("extensions.formautofill.addresses.enabled", false);
           user_pref("extensions.formautofill.creditCards.enabled", false);
+
           // no phoning home
           user_pref("extensions.htmlaboutaddons.recommendations", false);
           user_pref("browser.discovery.enabled", false);
@@ -57,24 +70,22 @@
           user_pref("browser.region.update.enabled", false);
           user_pref("browser.region.network.url", "");
           user_pref("geo.provider.network.url", "");
+
           // networking
           user_pref("network.prefetch-next", false);
           user_pref("network.IDN_show_punycode", true);
           user_pref("network.dns.echconfig.enabled", true);
+
           // history on, purged at shutdown by enterprise policy
           user_pref("places.history.enabled", true);
-          // i cannot sacrifice my eyes
-          user_pref("ui.systemUsesDarkTheme", 1);
-          // use RFP
+
+          // Anti-fingerprinting
           user_pref("privacy.resistFingerprinting", true);
+          user_pref("privacy.resistFingerprinting.pbmode", true);
           user_pref("privacy.resistFingerprinting.letterboxing", true);
-
+          user_pref("privacy.resistFingerprinting.skipEarlyBlankFirstPaint", true);
           user_pref("privacy.spoof_english", 2);
-
           user_pref("intl.accept_languages", "en-US, en");
-
-          // disable WebGL, unnecessary privacy & security attack vector
-          user_pref("webgl.disabled", true);
 
           // automatically reject cookies
           user_pref("cookiebanners.service.mode", 2);
